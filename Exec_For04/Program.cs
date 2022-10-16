4beea185a5f3cc1c7147ddbfc416115f1c01517f
yiru00 <eva890706@gmail.com>
2022-10-16 23:07:27 +0800

    作業

---
+++ Exec_For04/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            int foo = 3;
+            int bar = 5;
+            int foobar = 15;
+
+            if (i % foo == 0)
+            {
+                Console.WriteLine("foo");
+            }
+            else if (i % bar == 0)
+            {
+                Console.WriteLine("bar");
+            }
+            else if (i % foobar == 0)
+            {
+                Console.WriteLine("foobar");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
